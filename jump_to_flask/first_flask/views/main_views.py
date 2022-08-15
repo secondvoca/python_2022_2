cb827acc1081b455275069c41c24319939a300c5
zisei <secondvoca@gmail.com>
2022-08-15 16:09:44 +0900

    working on flask

---
+++ jump_to_flask/first_flask/views/main_views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+from first_flask.models import Question
+
+bp = Blueprint('main',
+               __name__,
+               url_prefix='/')
+
+
+@bp.route('/')
+def hello():
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('question/question_list.html', question_list=question_list)
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get_or_404(question_id)
+    return render_template('question/question_detail.html', question=question)
